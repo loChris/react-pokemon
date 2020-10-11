5917303dbc104a8cc57b12ac0291fe79c67f0496
loChris <44450518+loChris@users.noreply.github.com>
2020-10-11 15:50:59 -0400

    feature: Added better images called from another api; Had to also add a function which slices the id in order to properly call from the api.

--- src/pokecard/Pokecard.component.jsx
+++ src/pokecard/Pokecard.component.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import './Pokecard.styles.css';
 
-const POKE_API =
-	'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+// const POKE_API =
+//     'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+
+const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
+
+// To use the api we need to add slice digits because the api only recognizes 3 digits. Used in line 15.
+const padToThree = (num) => (num <= 999 ? `00${num}`.slice(-3) : num);
 
 export default class Pokecard extends Component {
 	render() {
 		const props = this.props;
-		let imgSrc = `${POKE_API}${props.id}.png`;
+		let imgSrc = `${POKE_API}${padToThree(props.id)}.png`;
 		return (
 			<div className="Pokecard">
 				<h1 className="Pokecard-title">{props.name}</h1>
